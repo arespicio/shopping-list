9de73207e4cf7c03509d15eef8843f5f374adfa2
Ashleigh Respicio <ashleighrespicio@gmail.com>
2015-02-26 10:15:12 -0500

    working on dynamic functions of list

--- js/app.js
+++ js/app.js
@@ -2,5 +2,4 @@ $(document).ready(function() {
 	$('#add-item').click(function() {
 		$('.produce-list').append('<li>' + $('#item').val() + '<i class="fa fa-times-circle show"></i></li>')  
 	});
-
 });
