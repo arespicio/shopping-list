95dc244926911be228a3d7689e8c20e408a9cf95
Ashleigh Respicio <ashleighrespicio@gmail.com>
2015-03-01 08:23:54 -0500

    trying to prevent form submission on enter button keydown

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function(){
+	
 	$(".hide").hide();
 
     //Adds item and category 
@@ -9,7 +10,14 @@ $(document).ready(function() {
 
         $('.' + catName).show(); //category
         $('.list-' + catName).append('<li class="ui-state-default"><span title="purchased">' + itemText + '</span><i class="fa fa-times-circle show" title="delete"></i>')//item
-	
+
+   $('#item').keydown(function(event){
+		if(key == 13) {
+			event.preventDefault();
+			return false;
+		};
+	});
+
     //resets item input area after "add item" clicked
         $(this).closest('form').find("input[type=text]").val("");
 	});
