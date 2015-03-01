0d0c4001207acd8fdf8003e3b8ffe62dcda14346
Ashleigh Respicio <ashleighrespicio@gmail.com>
2015-03-01 07:39:54 -0500

    added jquery ui plugin for sortable function

--- js/app.js
+++ js/app.js
@@ -5,16 +5,23 @@ $(document).ready(function() {
 	$('#add-item').click(function() {
         var catName = $(".category-dropdown option:selected").text();
         var itemText = $('#item').val();
+
+        if (itemText == '')  return;
+
         $('.' + catName).show(); //category
-        $('.list-' + catName).append('<li><span>' + itemText + '</span><i class="fa fa-times-circle show"></i>')//item
+        $('.list-' + catName).append('<li class="ui-state-default"><span title="purchased">' + itemText + '</span><i class="fa fa-times-circle show" title="delete"></i>')//item
 	});
 
 	//Removes list item when circle "delete" is clicked
 	$('.list').on('click', '.show', function(){
 		$(this).parent('li').remove();
 	});
-
+    
+    //Crosses off list item when clicked
 	$('.list').on('click', 'span', function(){
 		$(this).toggleClass('bought');
 	});
+
+	$('#sortable').sortable();
+	$('sortable').disableSelection();
 });
